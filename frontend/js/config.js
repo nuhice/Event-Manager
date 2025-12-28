b9236ccb45282a8b7c364ebc96719e129caa19a9
nuhice <enis.nuhic@stu.ibu.edu.ba>
2025-12-28 21:57:15 +0100

    fix5

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,7 @@
 const API_CONFIG = {
-    baseUrl: (window.ENV && window.ENV.API_URL) ? window.ENV.API_URL : 'http://localhost/eventmanager/backend'
+    baseUrl: (window.ENV && window.ENV.API_URL && window.ENV.API_URL !== '__API_URL_PLACEHOLDER__')
+        ? window.ENV.API_URL
+        : 'http://localhost/eventmanager/backend'
 };
 
 window.API_CONFIG = API_CONFIG;
